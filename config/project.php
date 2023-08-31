d309c16cff6c61fa19fe6cfe978d3f5da91c793b
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-08-31 16:08:49 +0200

    create project in config

---
+++ config/project.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    [
+        'name_project' => 'Laravel-DC-Comics',
+        'url_project' => 'https://github.com/Riccardo15Pinto/laravel-dc-comics',
+        'description_project' => 'Progetto sulla DC usando Laravel',
+        'type_project' => 'PHP/Laravel',
+    ],
+    [
+        'name_project' => 'Laravel-Migration-Seeder',
+        'url_project' => 'https://github.com/Riccardo15Pinto/laravel-migration-seeder',
+        'description_project' => 'Progetto basato sull\' apprendimento delle migration e sui seeder usando Laravel',
+        'type_project' => 'PHP/Laravel',
+    ],
+    [
+        'name_project' => 'Laravel-Primi-Passi',
+        'url_project' => 'https://github.com/Riccardo15Pinto/laravel-primi-passi',
+        'description_project' => 'Progetto basato sull\' apprendimento di Laravel',
+        'type_project' => 'PHP/Laravel',
+    ],
+];
