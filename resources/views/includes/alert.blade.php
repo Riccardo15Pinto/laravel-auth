67065b885204ed92b8507c630ba59a297a6bad3e
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-01 15:39:58 +0200

    add close button and margin in alert

--- resources/views/includes/alert.blade.php
+++ resources/views/includes/alert.blade.php
@@ -1,7 +1,11 @@
 @if (session('alert-message'))
-    <div class="container">
+    <div class="container my-3">
         <div class="alert alert-{{ session('alert-type') }}">
-            {{ session('alert-message') }}
+            <div class="d-flex align-items-center justify-content-between">
+
+                {{ session('alert-message') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
         </div>
     </div>
 @endif
